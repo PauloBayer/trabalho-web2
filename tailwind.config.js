14c1807837113d2ceeaaca58454e939115a0122c
Rodrigo do Vale Stankowicz <rodrigo.vale.st@gmail.com>
2024-09-13 17:02:12 -0300

    [add] nuckle font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        nuckle: ["nuckle"]
+      }
+    },
   },
   plugins: [],
 }
-
