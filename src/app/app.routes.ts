c053ffe02f359d8c783f846f0973061a987facd1
Patrick Alves Bastos <patrickalves@ufpr.br>
2024-09-15 19:09:39 -0300

    Tela de Cadastro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent} from './pages/register/register.component'; 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "register",
+        component: RegisterComponent
+    }
+  ];
