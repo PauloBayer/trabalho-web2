479cd31291bddd054fa71e3cb5a94e03d9a14307
Rodrigo do Vale Stankowicz <rodrigo.vale.st@gmail.com>
2024-10-20 18:11:53 -0300

    [add] redirecionar de acordo com a role e header pagina inicial main layout

--- frontend/src/app/layout/main-layout/main-layout.component.ts
+++ frontend/src/app/layout/main-layout/main-layout.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-main-layout',
@@ -9,10 +10,10 @@ import { Router, RouterModule } from '@angular/router';
   styleUrl: './main-layout.component.css',
 })
 export class MainLayoutComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
-  goHome() {
-    this.router.navigate(['']);
+  goHomepage() {
+    this.authService.navigateToHomepageByRole();
   }
   
   goLogin() {
@@ -21,5 +22,13 @@ export class MainLayoutComponent {
   
   goRegister() {
     this.router.navigate(['register']);
+  }
+
+  goLogout() {
+    this.router.navigate(['logout']);
+  }
+
+  isAuthenticated(): boolean {
+    return this.authService.isLoggedIn();
   }
 }
